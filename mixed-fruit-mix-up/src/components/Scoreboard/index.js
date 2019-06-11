78405d49b706a4c51568f156dd65197a165adfa9
Liam Condon <condonliamt@gmail.com>
2019-06-11 14:11:44 -0700

    small edit in scorecard component

--- mixed-fruit-mix-up/src/components/Scoreboard/index.js
+++ mixed-fruit-mix-up/src/components/Scoreboard/index.js
@@ -7,8 +7,8 @@ class Scoreboard extends Component {
     render() {
       return(
         <div>
-          <h2>Time: {this.props.time}</h2>
-          <h2>Score: {this.props.score}</h2>
+          <h2 className='time'>Time: <span>{this.props.time}</span></h2>
+          <h2 className='score'>Score: <span>{this.props.score}</span></h2>
         </div>
       );
     }
